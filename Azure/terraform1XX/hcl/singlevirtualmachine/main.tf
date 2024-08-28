d0bff1cf35850f04c7d7a5eb0f47e04d34637cdb
Gary Lopez <glopez@gbm.net>
2024-08-28 13:41:03 -0600

    Adding outputs

--- Azure/terraform1XX/hcl/singlevirtualmachine/main.tf
+++ Azure/terraform1XX/hcl/singlevirtualmachine/main.tf
@@ -20,7 +20,11 @@
 # Define the Azure provider
 #########################################################
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 #########################################################
